04567528df166272c93ea8de66da2f94aed497f8
Lauren Spatz <laurens914@gmail.com>
2016-02-15 22:33:53 -0800

    added playgorund work

---
+++ Homework Monday.playground/Contents.swift
@@ -0,0 +1,36 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+var test = "this is a test"
+var hawks = "Sehawks are the best team in the NLF"
+
+// this way sort of works, but you cant have additional spaces
+func totalWords (string:String) -> [String]
+{
+    let wordsArray  = string.componentsSeparatedByString(" ")
+    return wordsArray
+}
+totalWords(test).count
+totalWords(hawks).count
+
+//this way seems to work better than the previous way
+func wordCount(string: String) -> Int
+{
+    var wordsArray = string.characters
+    
+    let word = string.characters.filter { (wrds) -> Bool in
+        if wordsArray.last == " " {
+            wordsArray.removeLast()
+            return false
+        } else {
+        return wrds == " "
+        }
+    }
+    return word.count + 1
+}
+
+
+wordCount(hawks)
+wordCount(test)
